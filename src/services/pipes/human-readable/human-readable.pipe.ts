36d33ad1eae651fb2e4106253d7713e53e854bd8
Bito165 <fbabalola@sbsc.com>
2021-02-20 11:03:38 +0100

    Interceptors & Pipes

--- src/services/pipes/human-readable/human-readable.pipe.ts
+++ src/services/pipes/human-readable/human-readable.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
 @Pipe({
   name: 'humanReadable'
 })
 export class HumanReadablePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string) {
+    if ((typeof value) !== 'string') {
+      return value;
+    }
+    value = value.split(/(?=[A-Z])/).join(' ');
+    value = value[0].toUpperCase() + value.slice(1);
+    return value;
   }
 
 }
+
